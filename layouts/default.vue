3e5eaa2833bf761532618fdf90e9bcca58537d63
Sivar Sarkawt <sivar.sarkawt@tornet.co>
2023-06-27 19:29:16 +0300

    Layouts

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="DefaultLayout h-screen bg-slate-900 text-white">
+        <p class="text-red">Default layout</p>
+        <slot />
+    </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ pages/custom.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="CustomPage">Custom Page</div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "custom",
+})
+</script>
